d15d0ca5ebb36f324347e3a5dcb95f565c21cea2
theVeteranDeveloper <theveterandeveloper@gmail.com>
2025-04-02 21:02:24 -0600

    both directions

--- invertedPyramid.js
+++ invertedPyramid.js
@@ -1,7 +1,7 @@
-const character = "!";
-const count = 10;
+const character = "#";
+const count = 8;
 const rows = [];
-let inverted = false;
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
   return (
